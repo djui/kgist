6fd2b2f369e9619285787deff1e935488b61abf9
Uwe Dauernheim <uwe@klarna.com>
2011-05-04 17:20:54 +0200

    Pygments helper

---
+++ _sketches/pygments_languages2json.py
@@ -0,0 +1,55 @@
+import pygments.lexers as pl
+import locale
+import json
+
+wantedLanguages = [ "Bash"
+                  , "Clojure"
+                  , "CoffeeScript"
+                  , "Common Lisp"
+                  , "CSS"
+                  , "Diff"
+                  , "Erlang"
+                  , "Gettext Catalog"
+                  , "Gnuplot"
+                  , "Go"
+                  , "Haskell"
+                  , "HTML"
+                  , "INI"
+                  , "IRC logs"
+                  , "Java"
+                  , "JavaScript"
+                  , "Makefile"
+                  , "MySQL"
+                  , "Perl"
+                  , "PHP"
+                  , "Python"
+                  , "Python 3"
+                  , "reStructuredText"
+                  , "Ruby"
+                  , "S"
+                  , "Scala"
+                  , "SQL"
+                  , "TeX"
+                  , "Text only"
+                  , "VimL"
+                  , "XML"
+                  , "XQuery"
+                  , "XSLT"
+                  ]
+
+UnfilteredLexers = pl.get_all_lexers()
+Lexers = []
+for l in UnfilteredLexers:
+    if l[1] == ('basemake',): continue # Bugfix: we want unique keys (Makefile)
+    if not l[0] in wantedLanguages: continue
+    Lexers.append(l)
+sortLambda = lambda x: locale.strxfrm(x[0].lower())
+sortedLexers = sorted(Lexers, key=sortLambda)
+newLexers = []
+for l in sortedLexers:
+    newLexers.append(dict({'name':  l[0], 
+                           'alias': l[1],
+                           'ext':   l[2],
+                           'mime':  l[3]}))
+JSON = json.dumps(newLexers) # indent=2
+print JSON
