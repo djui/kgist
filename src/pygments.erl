daa637b9385857adadfe051f6901a0ebbe6ebf44
Uwe Dauernheim <uwe@klarna.com>
2011-09-28 22:10:37 +0200

    Add pygmentize server

--- src/pygments.erl
+++ src/pygments.erl
@@ -1,11 +1,61 @@
 -module(pygments).
+-behaviour(gen_server).
 
--export([ pygmentize/1
+%%% Exports ====================================================================
+-export([ start_link/0
+        , pygmentize/2
+        ]).
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
         ]).
 
-pygmentize(_Source) ->
-  nyi.
+%%% Records ====================================================================
+-record(state, { view_dir
+               , views
+               }).
 
+%%% Defines ====================================================================
+-define(SERVER, ?MODULE).
+
+%%% Code =======================================================================
+%%% API ------------------------------------------------------------------------
+pygmentize(CodeText, Language) ->
+  gen_server:call(?SERVER, {pygmentize, CodeText, Language}).
+
+%%% Callbacks ------------------------------------------------------------------
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+  Cmd   = os:find_executable("pygmentize"),
+  Flags = "",
+  {ok, #state{ cmd   = Cmd
+             , flags = Flags
+             }}.
+
+handle_call({pygmentize, CodeText, Language}, _From, State=#state{cmd=Cmd, flags=Flags}) ->
+  CodeHighlighted = os_cmd(Cmd ++ " " ++ string:join(" ", Flags) ++ " " ++ Language ++ " - " ++ CodeText),
+  {reply, CodeHighlighted, State};
+handle_call(_, _From, State) ->
+  {reply, undefined, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%% Internals ------------------------------------------------------------------
 os_cmd(Cmd) ->
   try
     Port = erlang:open_port({spawn_command, Cmd}, [exit_status]),
