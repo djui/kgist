aa454308f88d30dabe26c4abfc237d6e54359783
Uwe Dauernheim <uwe@klarna.com>
2011-10-03 14:01:22 +0200

    Use layout view as render partial for bodies

--- src/kgist_view.erl
+++ src/kgist_view.erl
@@ -2,9 +2,10 @@
 -behaviour(gen_server).
 
 %%% Exports ====================================================================
--export([ start_link/0
+-export([ start_link/1
         , reload/0
         , render/2
+        , render_partial/2
         ]).
 -export([ init/1
         , handle_call/3
@@ -16,6 +17,7 @@
 
 %%% Records ====================================================================
 -record(state, { view_dir
+               , layout_view
                , views
                }).
 
@@ -28,25 +30,37 @@
 render(ViewId, ViewCtx) ->
   gen_server:call(?SERVER, {render, ViewId, ViewCtx}).
 
+render_partial(ViewId, ViewCtx) ->
+  gen_server:call(?SERVER, {render_partial, ViewId, ViewCtx}).
+
 reload() ->
   gen_server:cast(?SERVER, reload).
 
 %%% Callbacks ------------------------------------------------------------------
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Config) ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, Config, []).
 
-init([]) ->
-  {ok, ViewDir} = application:get_env(view_dir),
-  Views         = reload_views(ViewDir),
-  {ok, #state{ view_dir = ViewDir
-             , views    = Views
+init(Config) ->
+  ViewDir    = proplists:get_value(view_dir, Config),
+  LayoutView = proplists:get_value(layout_view, Config),
+  Views      = reload_views(ViewDir),
+  {ok, #state{ view_dir    = ViewDir
+             , layout_view = LayoutView
+             , views       = Views
              }}.
 
-handle_call({render, ViewId, ViewCtx0}, _From, State=#state{views=Views}) ->
-  ViewCtx = case ?is_dict(ViewCtx0) of
-              true  -> ViewCtx0;
-              false -> dict:from_list(ViewCtx0)
-            end,
+handle_call({render, ViewId, ViewCtx0}, _From,
+            State=#state{views=Views, layout_view=LayoutViewId}) ->
+  ViewCtx    = ensure_dict(ViewCtx0),
+  View       = dict:fetch(ViewId, Views),
+  Body       = do_render(View, ViewCtx),
+  LayoutCtx  = dict:store(body, Body, ViewCtx),
+  LayoutView = dict:fetch(LayoutViewId, Views),
+  Content    = do_render(LayoutView, LayoutCtx),
+  {reply, Content, State};
+handle_call({render_partial, ViewId, ViewCtx0}, _From,
+            State=#state{views=Views}) ->
+  ViewCtx = ensure_dict(ViewCtx0),
   View    = dict:fetch(ViewId, Views),
   Content = do_render(View, ViewCtx),
   {reply, Content, State};
@@ -70,8 +84,7 @@ code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
 %%% Internals ------------------------------------------------------------------
-do_render(View, ViewCtx) ->
-  mustache:render(undefined, View, ViewCtx).
+do_render(View, ViewCtx) -> mustache:render(undefined, View, ViewCtx).
 
 reload_views(ViewDir) ->
   Load = fun(ViewFilename, Acc) ->
@@ -81,9 +94,12 @@ reload_views(ViewDir) ->
              View       = mustache:compile(ViewFile),
              dict:store(ViewName, View, Acc)
          end,
-  filelib:fold_files(ViewDir, ".*\.html$", false, Load, dict:new()).
+  filelib:fold_files(ViewDir, ".*\.html$", false, Load, dict:new()). %%"
 
 escape_parenthesis(B) when is_binary(B) ->
   binary:replace(B, <<"\"">>, <<"\\\"">>, [global]);
 escape_parenthesis(S) ->
   re:replace(S, "\"", "\\\\\"", [{return, list}, global]).
+
+ensure_dict(ViewCtx) when ?is_dict(ViewCtx) -> ViewCtx;
+ensure_dict(ViewCtx)                        -> dict:from_list(ViewCtx).
