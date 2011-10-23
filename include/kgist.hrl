c2a78ae65d7b722647cbeaec6e26461bb7f908c3
Uwe Dauernheim <uwe@klarna.com>
2011-10-23 22:12:57 +0200

    Remove crazy id generator

--- include/kgist.hrl
+++ include/kgist.hrl
@@ -1,9 +1,4 @@
 -define(GIST_TABLE, gist).
--define(GIST_KEY_COUNTER, 0).
-
-%% Size included as we can't include the record defintion from dict.erl
--define(is_dict(D), is_record(D, dict, 8)).
-
 -record(gist, { id
               , creation_time
               , expires
